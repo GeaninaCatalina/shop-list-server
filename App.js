d0a0c416b678aa80373d4073e6a7c5cc703b4ef2
GeaninaCatalina <masgras.geanina@hotmail.com>
2020-05-08 18:33:25 +0200

    Geanina: conected user table to express js

--- App.js
+++ App.js
@@ -9,21 +9,21 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 var dbUrl = 'mongodb://geanina:password1@ds155747.mlab.com:55747/shopping-list'; 
 
-mongoose.connect(dbUrl , { useNewUrlParser: true },  (err) => { 
+mongoose.connect(dbUrl , { useNewUrlParser: true, useUnifiedTopology: true },  (err) => { 
     console.log('data base connected',err);
- },
- { useUnifiedTopology: true }
- )
+ });
 
-const Users = mongoose.model('Users', { userName: String, pasword:String });
-const ShoppingLists = mongoose.model('ShoppingLists', { listName:String, products:String}); 
+const UsersConnector = mongoose.model('users', { userName: String, pasword:String });
+const ShoppingListsConnector = mongoose.model('shopping-lists', { listName:String, products:String}); 
+
+//const user = new UsersConnector({ userName: 'nume', pasword:'1234534' });
+//user.save()
 
 app.get('/login',(req, res)=> {
-    Users.find({}, (err, userName)=> {
-        console.log(req.body)
+    UsersConnector.find({}, (err, users)=> {
+        console.log(users)
         res.send(true);
     })
 })
-
 
 app.listen(4100, () => console.log(`You app is listening on port ${PORT}`));
